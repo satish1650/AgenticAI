fe40687b4973ae8e9b912a8fad1f364627183f3e
Satish Kumar <satishsjce86@gmail.com>
2025-07-21 19:18:55 +0000

    Code Executor Agent Output

---
+++ 4-AutoGen/4.8-Autogen Projects/4.8.1-Analyser GPT/4.8.1.0-CodeExecutorAgentOutput.lua
@@ -0,0 +1,13 @@
+--------------- Output-1 ---------------
+The result is Response(chat_message=
+TextMessage(
+    id='31dfa222-216f-43f7-a50a-3cd79ba55359',
+    source='CodeExecutorAgent', models_usage=None, metadata={}, created_at=datetime.datetime(2025, 7, 21, 17, 59, 49, 854537, tzinfo=datetime.timezone.utc),
+    content='Hello, World!\n', type='TextMessage'), inner_messages=None)
+
+--------------- Output-2 ---------------
+The result is Response(chat_message=
+TextMessage(
+    id='d54d4864-0610-42be-9568-4eb09b2cf1a7', 
+    source='CodeExecutorAgent', models_usage=None, metadata={}, created_at=datetime.datetime(2025, 7, 21, 18, 34, 14, 291326, tzinfo=datetime.timezone.utc), 
+    content='Hello, World, how are you?!\n', type='TextMessage'), inner_messages=None)
