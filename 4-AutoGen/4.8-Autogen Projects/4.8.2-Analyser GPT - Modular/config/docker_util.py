58f2e93b017486c910cf1b975d746d13702c1220
Satish Kumar <satishsjce86@gmail.com>
2025-07-26 18:38:34 +0000

    updated->image='amancevice/pandas'

--- 4-AutoGen/4.8-Autogen Projects/4.8.2-Analyser GPT - Modular/config/docker_util.py
+++ 4-AutoGen/4.8-Autogen Projects/4.8.2-Analyser GPT - Modular/config/docker_util.py
@@ -5,6 +5,7 @@ from config.constants import WORK_DIR_DOCKER, TIMEOUT_DOCKER
 def getDockerCommandLineCodeExecutor():
 
     docker= DockerCommandLineCodeExecutor(
+        image='amancevice/pandas',
         work_dir=WORK_DIR_DOCKER,
         timeout=TIMEOUT_DOCKER
     )
