b9ae3f013e382d28f2b28856f6e68144ef1a8946
Satish Kumar <satishsjce86@gmail.com>
2025-08-03 10:51:23 +0000

    03 Aug Day - 26 Autogen MCP Project + Ngrok + Notion + Lovable

---
+++ Material/AgenticAI/03 Aug Day - 26 Autogen MCP Project + Ngrok + Notion + Lovable/port.py
@@ -0,0 +1,25 @@
+from flask import Flask,jsonify
+import os
+from pyngrok import ngrok
+from flask_cors import CORS
+
+NGROK_AUTH_TOKEN='YOUR-TOKEN'
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/hello',methods=['GET'])
+def hello():
+    return jsonify({'message':'hello from Mac via Terminal'})
+
+
+if __name__=='__main__':
+    port = 7001
+    os.environ['FLASK_ENV'] = 'development'
+
+    ngrok.set_auth_token(NGROK_AUTH_TOKEN)
+    public_url = ngrok.connect(port)
+    print(f"Public URL:{public_url}/api/hello \n \n")
+
+    app.run(port=port)
