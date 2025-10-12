a54af9a034267880aa614f057afc1a9a5bf53f0d
Satish Tiwari <satishsjce86@gmail.com>
2025-10-12 10:20:42 +0530

    Complete Class Notes

--- Material/AgenticAI/Complete Class Notes.lua
+++ Material/AgenticAI/Complete Class Notes.lua
@@ -1,7 +1,6 @@
-=====================================================================================================================================================================================
-																AutoGen Agentic Framework
-=====================================================================================================================================================================================
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+========================================================================================================================================
+						AutoGen Agentic Framework
+========================================================================================================================================
 05 July Day - 17 AutoGen Part-I
 ********************************
 mailtomayankaggarwal@gmail.com
@@ -11,16 +10,17 @@ https://www.instagram.com/tech.mayankagg/
 
 https://www.instagram.com/tech.mayankagg/
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 06 July Day - 18 AutoGen Part-II
 ********************************
 https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0](https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 12 July Day - 19 AutoGen Tools & Teams
 **************************************
 [https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0](https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0)
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 13 July Day - 20 AutoGen Teams & Mini Project
 *********************************************
 https://www.krishnaik.in/courses - JULY02
@@ -65,8 +65,8 @@ https://youtu.be/b41YDlYoeWY?list=PLI2v4rh7YVhdzMPIrK0az-tfd0wSDbjhp
 https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0
 
 https://www.youtube.com/watch?v=Z0J4Hdo_d78
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 20 July Day - 22 Autogen - Project advanced
 ********************************************
 GitHub URL
@@ -81,8 +81,8 @@ https://youtu.be/Z0_f7KSuscI
 
 Complete n8n Masterclass: Build Al Agents & Automate Workflows (Zero to Hero)
 https://youtu.be/DkV7ztrhLh8
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 26 July Day - 23 Autogen Project Continued + Swarm
 **************************************************
 [https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0](https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0)
@@ -101,8 +101,8 @@ Exception Error Handling Video
 https://www.youtube.com/watch?v=zu9Hc9aSWFc&pp=0gcJCccJAYcqIYzv
 
 https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0/Autogen
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 27 July Day - 24 Autogen Project Continued + Swarm
 **************************************************
 https://github.com/openai/swarm?tab=readme-ov-file
@@ -116,8 +116,8 @@ https://docs.humanizeai.pro/openapi/humanization/gethumanizationresult
 filess.io
 
 https://microsoft.github.io/autogen/stable//reference/python/autogen_agentchat.agents.html#autogen_agentchat.agents.SocietyOfMindAgent
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 02 Aug Day - 25 Autogen RAG & MCP
 *********************************
 https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0/Autogen/Notes
@@ -138,8 +138,8 @@ Udemy Free Courses
 https://www.udemy.com/course/ai-powered-data-analytics-mastery-ai-tools-vibe-coding/?couponCode=FREELEARN
 
 https://www.youtube.com/watch?v=WZAL4SiW9e0
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 03 Aug Day - 26 Autogen MCP Project + Ngrok + Notion + Lovable
 **************************************************************
 MCP Server
@@ -150,7 +150,6 @@ https://ngrok.com/
 
 UV Package
 https://www.youtube.com/watch?v=FL10d0k3GAo
-
 
 MCP Server Tools
 https://microsoft.github.io/autogen/stable//reference/python/autogen_ext.tools.mcp.html#autogen_ext.tools.mcp.mcp_server_tools
@@ -169,11 +168,7 @@ https://notion-chat-tester.lovable.app
 
 Lovable
 https://lovable.dev/
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-=====================================================================================================================================================================================
-																n8n Agentic Framework
-=====================================================================================================================================================================================
 
 09 Aug Day - 27 n8n Fundamentals
 ********************************
@@ -187,8 +182,8 @@ n8n Youtube Video
 https://www.youtube.com/watch?v=DkV7ztrhLh8
 
 https://docs.google.com/forms/d/e/1FAIpQLScrTWJnpHDDEgYll4d5nVZpdt-O--Kl8u3WIPTmlBsl4GO3CQ/viewform
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 10 Aug Day - 28 n8n Advanced
 ****************************
 An open source load testing tool.
@@ -199,13 +194,13 @@ https://elevenlabs.io/
 
 n8n Youtube Video
 https://www.youtube.com/watch?v=DkV7ztrhLh8
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 16 Aug Day - 29 n8n Doubt Session
 *********************************
 n8n Doubt Session
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 17 Aug Day - 30 n8n Project (Telegram)
 **************************************
 https://hub.docker.com/r/n8nio/n8n
@@ -218,6 +213,7 @@ docker.n8n.io/n8nio/n8n
 
 https://telegram.me/BotFather
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 23 Aug Day - 31 n8n Advanced Project
 ***********************************
 https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0/n8n
@@ -239,6 +235,7 @@ const combinedResults = items.map((item) => {
 });
 return combinedResults;
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 24 Aug Day - 32 n8n Advanced Project Multi-Agent
 ************************************************
 https://www.educative.io/courses/grokking-the-system-design-interview?utm_campaign=system_design&utm_source=google&utm_medium=ppc&utm_content=search&utm_term=course&eid=5082902844932096&utm_term=grokking%20the%20system%20interview&utm_campaign=%5BNew%5D+System+Design-Search-+Exc+US+CN+IND&utm_source=adwords&utm_medium=ppc&hsa_acc=5451446008&hsa_cam=18181328148&hsa_grp=178751266987&hsa_ad=735444448508&hsa_src=g&hsa_tgt=kwd-903484928147&hsa_kw=grokking%20the%20system%20interview&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=18181328148&gbraid=0AAAAADfWLuQaIBeZ9ZPHDOQ9RFN4VqfRH&gclid=Cj0KCQjwzaXFBhDlARIsAFPv-u_nN4CgDSuhmmUCMNsQqSpFFMUFumy7swehB1qxOEZxKP-iYVWYVdcaArd0EALw_wcB
@@ -254,8 +251,8 @@ https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0/n8n/29-30th%20Au
 https://www.youtube.com/watch?v=AnOETYkdQEs
 
 https://supabase.com/docs/guides/ai/langchain?database-method=sql
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 31 Aug Day - 34 n8n E2E Projects + n8n Local Setup + RAG/MCP + Nano-Banana App
 ******************************************************************************
 https://github.com/n8n-io/self-hosted-ai-starter-kit
@@ -270,8 +267,9 @@ https://aistudio.google.com/apikey
 
 https://kie.ai/logs
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-06 Sep Day - 35 ADK - Basics Introduction
-*****************************************
+
+06 Sep Day - 35 Google ADK (Agent Development Kit) - Basics
+***********************************************************
 Youtube
 https://www.youtube.com/@tech.mayankagg
 
@@ -288,26 +286,26 @@ mailtomayankaggarwal@gmail.com
 ADK Documents
 https://google.github.io/adk-docs/
 
-
 Google AI Studio
 https://aistudio.google.com/prompts/new_chat
 
 Google AI Studio -> Console
 https://console.cloud.google.com/welcome
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 07 Sep Day - 36 Google ADK (Agent Development Kit) - Advanced 
 *************************************************************
 https://google.github.io/adk-docs/get-started/quickstart/#agentpy
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 13 Sep Day - 37 Google ADK - Mini Project
 *****************************************
 https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0
 
 Show Graph in jupyter
 https://marketplace.visualstudio.com/items?itemName=bierner.markdown-mermaid
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 14 Sep Day - 38 Google ADK - Project-Enhance + Advance Concepts
 ***************************************************************
 ADK Accuracy
@@ -319,9 +317,8 @@ https://github.com/mayank953/Live-Class/tree/main/Agentic%202.0
 https://cloud.google.com/blog/products/ai-machine-learning/multimodal-generative-ai-search
 
 https://google.github.io/adk-docs/observability/logging/#debugging-with-logs-a-practical-example
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 20 Sep Day - 39 Google ADK - Project-Part-1 + Local Development Using FastAPI + Postman
 ***************************************************************************************
 https://bugs-sleep-6uj.craft.me/WPCzMdQXXN51kL
@@ -334,8 +331,8 @@ https://docs.google.com/document/d/1rsaK53T3Lg5KoGwvf8ukOUvbELRtH-V0LnOIFDxBryE/
 
 mailtomayankaggarwal@gmail.com
 aiwithmayank@gmail.com
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 21 Sep Day - 40 Google ADK - Project-Part-2 + Stremlit Development + Deploying Agent in ADK using Colud Run
 ***********************************************************************************************************
 Google Cloud Account Creation
@@ -345,21 +342,20 @@ Google Cloud CLI
 https://cloud.google.com/sdk/docs/install#mac
 
 https://capital-service-479047999858.us-central1.run.app
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 27 Sep Day - 41 Google ADK - Vertex AI deployment
 *************************************************
 https://google.github.io/adk-docs/deploy/agent-engine/#deploy-to-agent-engine
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 28 Sep Day - 42 Google ADK - Vertex AI deployment + E2E Project + Vertex AI RAG Engine
 **************************************************************************************
 https://drive.google.com/file/d/102Fe_6ExPSc5xEvKJaYenlUWAhlrPvcM/view?usp=sharing
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-04 Oct Day - 43 CrewAI - Crewai Part-1
-**************************************
+04 Oct Day - 43 CrewAI - Crewai Part-1 (Basics)
+***********************************************
 https://docs.crewai.com/en/introduction
 
 https://docs.crewai.com/en/installation
@@ -376,8 +372,8 @@ GitHub -> Sourangshu Pal
 https://gist.github.com/sourangshupal/406e7380ac4bfbd38df6eb7cd27f38c5
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-05 Oct Day - 44 CrewAI - Crewai Part-2
-**************************************
+05 Oct Day - 44 CrewAI - Crewai Part-2 (Advanced->Knowledge)
+************************************************************
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 https://github.com/sourangshupal/crewai-advanced
 
@@ -386,13 +382,12 @@ https://gist.github.com/sourangshupal/406e7380ac4bfbd38df6eb7cd27f38c5
 
 https://gist.github.com/sourangshupal/bd9d1d232a7dde1d492cfd9c71aaca7a
 
-
 Crewai with OPIK Integration.ipynb
 https://colab.research.google.com/drive/1fx_JOzNB95mf91BMVQgQdSuShHD6L4Bz?usp=sharing
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-11 Oct Day - 46 CrewAI - Crewai Part-3
-**************************************
+11 Oct Day - 46 CrewAI - Crewai Part-3 (Memory+Mem(0)+Trip-Planner-using-CrewAI)
+********************************************************************************
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 https://github.com/sourangshupal/crewai-advanced
 
@@ -429,4 +424,3 @@ ollama run llama3.2
 ************************
 Holiday's for Diwali
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
